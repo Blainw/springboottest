1de71064ec2707220615fb2c5f78e0f8e1408eb2
wdm <1831486177@qq.com>
2022-07-04 11:44:56 +0800

    first commit

---
+++ src/main/java/wdm/springboot/Controller.java
@@ -0,0 +1,12 @@
+package wdm.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("/hello")
+    public String test(){
+        return "你好，springboot";
+    }
+}
