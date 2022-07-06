699efe699ee0875fcfccec973c9b07788ee6df64
wdm <1831486177@qq.com>
2022-07-06 14:20:14 +0800

    commit1

--- src/main/java/wdm/springboot/ch3/ch3_2/Car.java
+++ src/main/java/wdm/springboot/ch3/ch3_2/Car.java
@@ -3,7 +3,7 @@ package wdm.springboot.ch3.ch3_2;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-          //加这个注解是为了让此类注册到Ioc容器中，只有在容器中，才能使用如下注解进行注入
+
 @ConfigurationProperties(prefix = "mycar1")     //按照配置文件进行注入，通过前缀mycar搜索key，搜索到后拿类中的属性去匹配key的后缀，最终确定关系，进行注入
 public class Car {
     private String brand;
